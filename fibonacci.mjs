0f5b5d5f625252a2c19c47e9469ada125a1b0644
miraejang <mirae611@nate.com>
2023-01-23 02:38:54 +0900

    Memozation - Fibonacci

---
+++ fibonacci.mjs
@@ -0,0 +1,24 @@
+function fibonacci1(n) {
+  if (n === 1 || n === 0) return n;
+  return fibonacci1(n - 2) + fibonacci1(n - 1);
+}
+
+function fibonacci2(n, memo) {
+  if (n === 1 || n === 0) return n;
+
+  if (memo[n] === undefined) {
+    memo[n] = fibonacci2(n - 2, memo) + fibonacci2(n - 1, memo);
+  }
+
+  return memo[n];
+}
+
+let start = new Date();
+console.log(fibonacci1(40));
+let end = new Date();
+console.log(`fibonacci1 실행시간: ${end - start}ms`);
+
+start = new Date();
+console.log(fibonacci2(40, {}));
+end = new Date();
+console.log(`fibonacci2 실행시간: ${end - start}ms`);

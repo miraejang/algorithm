3da07aa62028412e1cd5a4d3f4cd3eeb6aaabdf9
miraejang <mirae611@nate.com>
2023-01-23 01:07:53 +0900

    Merge Sort

---
+++ merge_sort.mjs
@@ -0,0 +1,47 @@
+function MergeSort(arr, leftIndex, rightIndex) {
+  if (leftIndex < rightIndex) {
+    const midIndex = Math.floor((leftIndex + rightIndex) / 2);
+    MergeSort(arr, leftIndex, midIndex);
+    MergeSort(arr, midIndex + 1, rightIndex);
+    Merge(arr, leftIndex, midIndex, rightIndex);
+  }
+}
+
+function Merge(arr, leftIndex, midIndex, rightIndex) {
+  let leftAreaIndex = leftIndex;
+  let rightAreaIndex = midIndex + 1;
+  const tempArr = new Array(rightIndex + 1).fill(0);
+  let tempArrIndex = leftIndex;
+
+  while (leftAreaIndex <= midIndex && rightAreaIndex <= rightIndex) {
+    if (arr[leftAreaIndex] <= arr[rightAreaIndex]) {
+      tempArr[tempArrIndex++] = arr[leftAreaIndex++];
+    } else {
+      tempArr[tempArrIndex++] = arr[rightAreaIndex++];
+    }
+  }
+
+  if (leftAreaIndex > midIndex) {
+    for (let i = rightAreaIndex; i <= rightIndex; i++) {
+      tempArr[tempArrIndex++] = arr[i];
+    }
+  } else {
+    for (let i = leftAreaIndex; i <= midIndex; i++) {
+      tempArr[tempArrIndex++] = arr[i];
+    }
+  }
+
+  for (let i = leftIndex; i <= rightIndex; i++) {
+    arr[i] = tempArr[i];
+  }
+}
+
+const arr = [3, 5, 2, 4, 1, 7, 8, 6];
+
+console.log("===== 정렬 전 =====");
+console.log(arr);
+
+MergeSort(arr, 0, arr.length - 1);
+
+console.log("===== 정렬 후 =====");
+console.log(arr);
